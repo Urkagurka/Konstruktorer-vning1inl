0c04e12db75478e94f1e162e052ad8fcb075ddc7
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-04-05 13:13:46 +0200

    Add project files.

---
+++ Konstruktorerövning1inl/BILREKGISLTER.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktorerövning1inl
+{
+    class BILREKGISLTER
+    {
+        public string brand;
+        public string model;
+        public string hp;
+        public string enginedisplacement;
+        public string year;
+    }
+}
---
+++ Konstruktorerövning1inl/Program.cs
@@ -0,0 +1,32 @@
+﻿using Konstruktorerövning1inl;
+using System;
+
+namespace Konstruktörerövning1inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BILREKGISLTER p1 = new BILREKGISLTER();
+            Console.WriteLine("Vilket märke är bilen?");
+            p1.brand = Console.ReadLine();
+            Console.WriteLine("Vilken model är bilen?");
+            p1.model = Console.ReadLine();
+            Console.WriteLine("Hur många hästkrafter har bilen?");
+            p1.hp = Console.ReadLine();
+            Console.WriteLine("Hur många cc är motorn?");
+            p1.enginedisplacement = Console.ReadLine();
+            Console.WriteLine("Vilket år byggdes bilen?");
+            p1.year = Console.ReadLine();
+
+            Console.Write(p1.brand + " ");
+            Console.WriteLine(p1.model);
+            Console.WriteLine("Hästkrafter: " + p1.hp);
+            Console.WriteLine("Motor storlek: " + p1.enginedisplacement);
+            Console.WriteLine("År: " + p1.year);
+            Console.WriteLine("");
+
+
+        }
+    }
+}
